79e4a5933f4014d22bacdf4bab2f5caeb9259263
cs-py <e.1@mail.ru>
2021-02-28 17:21:10 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,3 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  return matrix ? matrix.reduce((acc, el, i) => acc.concat(i%2 ? el.reverse() : el), []) : []
 }
